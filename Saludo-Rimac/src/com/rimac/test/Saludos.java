f87b760c8e68876012fdb7ce5703d4ae1b7eab2c
Christian Sanchez <christian.sanchez@rimac.com.pe>
2012-06-25 17:08:00 -0500

    Primera prueba usando GitHub

---
+++ Saludo-Rimac/src/com/rimac/test/Saludos.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.rimac.test;
+
+/**
+ * @author csanchezgu
+ *
+ */
+public class Saludos {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hola Rimac!!!");
+	}
+
+}
