9445bd895396b73bde21bca62fb60bda049bb1f8
Christian Sanchez <christiansanchezgutierrez@gmail.com>
2012-07-01 19:33:50 -0500

    Se realiza cambio desde Home para realizar pruebas con GITHUB

--- Saludo-Rimac/src/com/rimac/test/Saludos.java
+++ Saludo-Rimac/src/com/rimac/test/Saludos.java
@@ -15,6 +15,7 @@ public class Saludos {
 	public static void main(String[] args) {
 		System.out.println("Hola Rimac!!!");
 		System.out.println("Todo va a estar bien!!!");
+		System.out.println("Primera prueba con autenticacion ssh");
 	}
 
 }
